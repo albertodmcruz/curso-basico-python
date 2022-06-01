2ac1490e2bde94d7c9d968795741714c20f46c35
albertodmcruz <91384992+albertodmcruz@users.noreply.github.com>
2022-06-01 08:21:23 -0500

    initial commit

---
+++ calificaciones.py
@@ -0,0 +1,8 @@
+calificaciones=int(input('Introduce tu calificación de la AZ-900: '))
+
+if calificaciones<700:
+    print('Vees, por no estudiar')
+elif calificaciones>1000:
+    print('MIENTES! No puedes sacar más de mil')
+else:
+    print('Felicidades, pasa por tu certificado')
